c2146eddd4e63ec4c7e1c8972f0fdf952ab8f787
dolphinDoReMi <dolphinDoReMi@gmail.com>
2016-04-21 15:12:34 -0700

    [Merged] merged updated common page:navigation

--- meteor/client/views/pages/dashboards/Paper.js
+++ meteor/client/views/pages/dashboards/Paper.js
@@ -1,3 +1,19 @@
+Template.dashboard1.helpers({
+
+    // For demo purpose
+    // Example of notification list
+    notifications : [
+        {number: 1, labelClass: 'success', content: 'Please contact me', time: '09:00 pm'},
+        {number: 2, labelClass: 'info', content: 'Sign a contract', time: '10:16 am'},
+        {number: 3, labelClass: 'primary', content: 'Open new shop', time: '08.22 pm'},
+        {number: 4, labelClass: 'default', content: 'Call back to Sylvia', time: '11:06 pm'},
+        {number: 5, labelClass: 'primary', content: 'Write a letter to Sandra', time: '12:00 pm'}
+    ]
+
+    
+
+});
+
 Template.dashboard1.rendered = function(){
 
     // Set white background color for top navbar
@@ -28,7 +44,7 @@ Template.dashboard1.rendered = function(){
 
     var dataset = [
         {
-            label: "Number of orders",
+            label: "Number of total views",
             data: data3,
             color: "#1ab394",
             bars: {
@@ -39,7 +55,7 @@ Template.dashboard1.rendered = function(){
             }
 
         }, {
-            label: "Payments",
+            label: "Views",
             data: data2,
             yaxis: 2,
             color: "#1C84C6",
@@ -147,9 +163,24 @@ Template.dashboard1.rendered = function(){
             }]
         }
     });
-};
 
-Template.dashboard2.destroyed = function(){
-    // Remove special class
-    $('body').removeClass('light-navbar');
+    // Stocked horizontal bar
+
+    new Chartist.Bar('#ct-chart4', {
+        labels: ['Air China', 'Emirates', 'China Southern', 
+        'China Eastern', 'Star Alliance', 'Singapore Airlines',
+        'Hainan Airlines', 'Lufthansa', 'Cathay Pacific',
+        'Sky Team Alliance'],
+        series: [
+            [0.66, 0.32, 0.21, 0.20, 0.18, 
+            0.15, 0.13, 0.12, 0.11, 0.10]
+        ]
+    }, {
+        seriesBarDistance: 2,
+        reverseData: true,
+        horizontalBars: true,
+        axisY: {
+            offset: 70
+        }
+    });
 };
