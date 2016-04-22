0106633a199cc5df0eef21188fc128f5787eaad8
YanningLiu <13436529109@126.com>
2016-04-21 20:52:34 -0700

    meteor connect cloud database

--- meteor/server/main.js
+++ meteor/server/main.js
@@ -1,5 +1,11 @@
-Friends = new Mongo.Collection('friends');
-User2 = new Mongo.Collection('usersdatabase');
+//Friends = new Mongo.Collection('friends');
+//User2 = new Mongo.Collection('usersdatabase');
+
+// mongodb://user:pass@host:port/dbname
+
+var database = new MongoInternals.RemoteCollectionDriver("mongodb://root:root@ec2-54-200-79-239.us-west-2.compute.amazonaws.com:27017/test");
+Friends = new Mongo.Collection("friends", { _driver: database });
+User2 = new Mongo.Collection('usersdatabase', { _driver: database });
 
   Friends.allow({
     'insert': function (userId,doc) {
