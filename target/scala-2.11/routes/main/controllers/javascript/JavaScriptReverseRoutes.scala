0e668c78ff4c56712c9a86b572a57097233936a0
helunwencser <lunwenh@andrew.cmu.edu>
2016-04-22 09:56:23 -0700

    restore version1

---
+++ target/scala-2.11/routes/main/controllers/javascript/JavaScriptReverseRoutes.scala
@@ -0,0 +1,209 @@
+
+// @GENERATOR:play-routes-compiler
+// @SOURCE:/home/lunwenh/workspace/SSNetwork/conf/routes
+// @DATE:Fri Apr 22 00:52:24 PDT 2016
+
+import play.api.routing.JavaScriptReverseRoute
+import play.api.mvc.{ QueryStringBindable, PathBindable, Call, JavascriptLiteral }
+import play.core.routing.{ HandlerDef, ReverseRouteContext, queryString, dynamicString }
+
+
+import _root_.controllers.Assets.Asset
+import _root_.play.libs.F
+
+// @LINE:6
+package controllers.javascript {
+  import ReverseRouteContext.empty
+
+  // @LINE:30
+  class ReversePostController(_prefix: => String) {
+
+    def _defaultPrefix: String = {
+      if (_prefix.endsWith("/")) "" else "/"
+    }
+
+  
+    // @LINE:31
+    def getPostById: JavaScriptReverseRoute = JavaScriptReverseRoute(
+      "controllers.PostController.getPostById",
+      """
+        function(postId0) {
+          return _wA({method:"GET", url:"""" + _prefix + { _defaultPrefix } + """" + "post/getPostById/" + (""" + implicitly[PathBindable[Long]].javascriptUnbind + """)("postId", postId0)})
+        }
+      """
+    )
+  
+    // @LINE:35
+    def setAnswer: JavaScriptReverseRoute = JavaScriptReverseRoute(
+      "controllers.PostController.setAnswer",
+      """
+        function() {
+          return _wA({method:"POST", url:"""" + _prefix + { _defaultPrefix } + """" + "post/setAnswer"})
+        }
+      """
+    )
+  
+    // @LINE:30
+    def addPost: JavaScriptReverseRoute = JavaScriptReverseRoute(
+      "controllers.PostController.addPost",
+      """
+        function() {
+          return _wA({method:"POST", url:"""" + _prefix + { _defaultPrefix } + """" + "post/addPost"})
+        }
+      """
+    )
+  
+    // @LINE:32
+    def getAllPosts: JavaScriptReverseRoute = JavaScriptReverseRoute(
+      "controllers.PostController.getAllPosts",
+      """
+        function() {
+          return _wA({method:"GET", url:"""" + _prefix + { _defaultPrefix } + """" + "post/getAllPosts"})
+        }
+      """
+    )
+  
+    // @LINE:33
+    def addComment: JavaScriptReverseRoute = JavaScriptReverseRoute(
+      "controllers.PostController.addComment",
+      """
+        function() {
+          return _wA({method:"POST", url:"""" + _prefix + { _defaultPrefix } + """" + "post/addComment"})
+        }
+      """
+    )
+  
+    // @LINE:34
+    def setAsQuestion: JavaScriptReverseRoute = JavaScriptReverseRoute(
+      "controllers.PostController.setAsQuestion",
+      """
+        function() {
+          return _wA({method:"POST", url:"""" + _prefix + { _defaultPrefix } + """" + "post/setAsQuestion"})
+        }
+      """
+    )
+  
+  }
+
+  // @LINE:24
+  class ReverseForumController(_prefix: => String) {
+
+    def _defaultPrefix: String = {
+      if (_prefix.endsWith("/")) "" else "/"
+    }
+
+  
+    // @LINE:24
+    def addComment: JavaScriptReverseRoute = JavaScriptReverseRoute(
+      "controllers.ForumController.addComment",
+      """
+        function() {
+          return _wA({method:"POST", url:"""" + _prefix + { _defaultPrefix } + """" + "comment"})
+        }
+      """
+    )
+  
+    // @LINE:26
+    def updateComment: JavaScriptReverseRoute = JavaScriptReverseRoute(
+      "controllers.ForumController.updateComment",
+      """
+        function() {
+          return _wA({method:"POST", url:"""" + _prefix + { _defaultPrefix } + """" + "updateComment"})
+        }
+      """
+    )
+  
+    // @LINE:25
+    def getComments: JavaScriptReverseRoute = JavaScriptReverseRoute(
+      "controllers.ForumController.getComments",
+      """
+        function(rootid0,categoryid1) {
+          return _wA({method:"GET", url:"""" + _prefix + { _defaultPrefix } + """" + "comment" + _qS([(""" + implicitly[QueryStringBindable[Integer]].javascriptUnbind + """)("rootid", rootid0), (""" + implicitly[QueryStringBindable[Integer]].javascriptUnbind + """)("categoryid", categoryid1)])})
+        }
+      """
+    )
+  
+    // @LINE:27
+    def uploadFile: JavaScriptReverseRoute = JavaScriptReverseRoute(
+      "controllers.ForumController.uploadFile",
+      """
+        function() {
+          return _wA({method:"POST", url:"""" + _prefix + { _defaultPrefix } + """" + "upload"})
+        }
+      """
+    )
+  
+  }
+
+  // @LINE:9
+  class ReverseAssets(_prefix: => String) {
+
+    def _defaultPrefix: String = {
+      if (_prefix.endsWith("/")) "" else "/"
+    }
+
+  
+    // @LINE:9
+    def at: JavaScriptReverseRoute = JavaScriptReverseRoute(
+      "controllers.Assets.at",
+      """
+        function(file1) {
+          return _wA({method:"GET", url:"""" + _prefix + { _defaultPrefix } + """" + "assets/" + (""" + implicitly[PathBindable[String]].javascriptUnbind + """)("file", file1)})
+        }
+      """
+    )
+  
+  }
+
+  // @LINE:6
+  class ReverseApplication(_prefix: => String) {
+
+    def _defaultPrefix: String = {
+      if (_prefix.endsWith("/")) "" else "/"
+    }
+
+  
+    // @LINE:20
+    def getPaperByTitle: JavaScriptReverseRoute = JavaScriptReverseRoute(
+      "controllers.Application.getPaperByTitle",
+      """
+        function(title0) {
+          return _wA({method:"GET", url:"""" + _prefix + { _defaultPrefix } + """" + "title/" + (""" + implicitly[PathBindable[String]].javascriptUnbind + """)("title", encodeURIComponent(title0))})
+        }
+      """
+    )
+  
+    // @LINE:19
+    def getPaperByYear: JavaScriptReverseRoute = JavaScriptReverseRoute(
+      "controllers.Application.getPaperByYear",
+      """
+        function(year0) {
+          return _wA({method:"GET", url:"""" + _prefix + { _defaultPrefix } + """" + "year/" + (""" + implicitly[PathBindable[Integer]].javascriptUnbind + """)("year", year0)})
+        }
+      """
+    )
+  
+    // @LINE:11
+    def upload: JavaScriptReverseRoute = JavaScriptReverseRoute(
+      "controllers.Application.upload",
+      """
+        function() {
+          return _wA({method:"POST", url:"""" + _prefix + { _defaultPrefix } + """" + "person/uploadPic"})
+        }
+      """
+    )
+  
+    // @LINE:6
+    def index: JavaScriptReverseRoute = JavaScriptReverseRoute(
+      "controllers.Application.index",
+      """
+        function() {
+          return _wA({method:"GET", url:"""" + _prefix + """"})
+        }
+      """
+    )
+  
+  }
+
+
+}
