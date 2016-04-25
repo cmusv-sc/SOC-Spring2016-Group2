42cad85d50b8a4fe7373bd4554da179c65ab881b
helunwencser <lunwenh@andrew.cmu.edu>
2016-04-24 22:29:27 -0700

    update post page

--- frontend/meteor/client/views/pages/postPage/postPage.js
+++ frontend/meteor/client/views/pages/postPage/postPage.js
@@ -1,34 +1,19 @@
-    Template.postPage.onRendered(function() {
-        $('#searchPostForm').submit(function(event) {
-            event.preventDefault();
-            var keyword = $('#keyword').val();
-            console.log(keyword);
-            HTTP.call(
-                'GET',
-                'http://localhost:9000/post/search/' + keyword,
-                {},
-                function(error, response) {
-                    if(error) {
-                        console.log(error);
-                    } else {
-                        console.log(response);
-                    }
+Template.postPage.onRendered(function() {
+    $('#searchPostForm').submit(function(event) {
+        event.preventDefault();
+        var keyword = $('#keyword').val();
+        console.log(keyword);
+        HTTP.call(
+            'GET',
+            'http://localhost:9000/post/search/' + keyword,
+            {},
+            function(error, response) {
+                if(error) {
+                    console.log(error);
+                } else {
+                    console.log(response);
                 }
-            );
-        });
-    });
-
-if(Meteor.is_server) {
-    Meteor.startup(function() {
-        Meteor.methods({
-            searchPost: function(keyword) {
-                var response = HTTP.call(
-                    'GET',
-                    'http://localhost:9000/post/search/' + keyword,
-                    {}
-                );
-                return response;
             }
-        });
+        );
     });
-}
+});
