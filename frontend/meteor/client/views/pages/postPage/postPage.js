a4507db232a3d4ea33adef0f1133db449204a3a5
helunwencser <lunwenh@andrew.cmu.edu>
2016-04-24 22:45:24 -0700

    update post pate

--- frontend/meteor/client/views/pages/postPage/postPage.js
+++ frontend/meteor/client/views/pages/postPage/postPage.js
@@ -16,4 +16,26 @@ Template.postPage.onRendered(function() {
             }
         );
     });
+    
+    function showResults(results) {
+        var postsContainer = $('#postsContainer');
+        if(results.length <= 0) {
+        	postsContainer.empty();
+			$('<p>', {
+				className: 'noRecordsFound',
+				html: 'No Results Were Found!'
+			}).hide().appendTo(postsContainer).fadeIn();
+        } else {
+			var pageContainer = $('<div>', {
+				className: 'pageContainer'
+			});
+			for (var i = 0; i < results.length; i++) {
+				pageContainer.append(new result(results[i]) + '');
+			}
+            resultsDiv.empty();
+			pageContainer.append('<div class="clear"></div>')
+				.hide().appendTo(resultsDiv)
+				.fadeIn('slow');
+        }
+    }
 });
