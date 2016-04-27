66096b2742719060ebb5eb255c153d72ec0d48c1
helunwencser <lwhecser@gmail.com>
2016-04-27 00:01:03 -0700

    add bootstrap into postPage

--- meteor/client/views/pages/postPage/postPage.js
+++ meteor/client/views/pages/postPage/postPage.js
@@ -1,87 +1,40 @@
 Template.postPage.events({
 
-	'click #findPost': function(event) {
+	'click .findPost': function(event) {
 		event.preventDefault();
-		Session.set('showAddNewPost', false);
+		Session.set('showSearchPost', true);
 	},
 
-	'click #addPost': function(event) {
-		Session.set('showAddNewPost', true);
-	},
-
-	'submit #searchPostForm': function(event) {
-		event.preventDefault();
-		var keyword = event.target.keyword.value;
-		console.log(keyword);
-        HTTP.call(
-                'GET',
-                'http://localhost:9000/post/search/' + keyword,
-                {},
-                function(error, response) {
-                    if(error) {
-                    	alert(error);
-                        console.log(error);
-                    } else {
-                        console.log(response);
-                    	if(response.length <= 0) {
-                    		alert("No record found");
-                    	} else {
-                    		Meteor.call('removeAllPosts');
-                    		for(var i = 0; i < response.length; i++) {
-                    			console.log(response[i].title);
-                    			console.log(response[i].authorId);
-                    			console.log(response[i].postAt);
-                    			console.log(response[i].content);
-                    			Posts.insert({
-                    				title: response[i].title,
-                    				author: response[i].authorId,
-                    				postAt: new Date(response[i].postAt),
-                    				content: response[i].content
-                    			});
-                    		}
-                    	}
-                    }
-                }
-            );
-	},
-
-	'submit #addPostForm': function(event) {
-		event.preventDefault();
-		var title = event.target.postTitle.value;
-		var content = event.target.postContent.value;
-		console.log(title);
-		console.log(content);
-		Meteor.call('removeAllPosts');
-		var post = {
-				title: title,
-				author: Meteor.userId(),
-				content: content,
-				postAt: new Date()
-			};
-		Posts.insert(post);
-		HTTP.call(
-				'POST',
-				'http://localhost:9000/post/addPost',
-				post,
-				function(error, response) {
-					if(error) {
-						console.log(error);
-						alert(error);
-					} else {
-						console.log(response);
-					}
-				}
-		);
+	'click .newPost': function(event) {
+		Session.set('showSearchPost', false);
 	}
 });
 
+var fetchData = function(url){
+	var contentholder = $("#content");
+	contentholder.remove();
+	$(".loading").append($("<div class='sk-spinner sk-spinner-cube-grid'><div class='sk-cube'></div><div class='sk-cube'></div><div class='sk-cube'></div><div class='sk-cube'></div><div class='sk-cube'></div><div class='sk-cube'></div><div class='sk-cube'></div><div class='sk-cube'></div><div class='sk-cube'></div></div>"));
+	Meteor.call('fetchFromService', url, function (err, res){
+		var obj = JSON.stringify(res.data);
+		$(".loading").empty();
+		$(".table").append("<tbody id='content'></tbody>");
+		$("#resultcount").text("Found " + res.data.length + " results.");
+		contentholder = $("#content");
+		for (var i = 0; i < res.data.length; i++) {
+			var row = "<tr><td><span class='label label-primary'>" + (i+1) + "</span> </td><td><a href='/paperdetail?id=" + res.data[i].post.id + "'>" + res.data[i].post.title + "</a>" + "<br><small>";
+			row += res.data[i].authorId;
+			row += "</small></td></tr>";
+			contentholder.append($(row));
+		};
+	});
+}
+
 Template.postPage.helpers({
 	ifShowAddNewPost: function() {
-		return Session.get('showAddNewPost');
+		return Session.get('showSearchPost');
 	},
-	posts: function() {
-		console.log("getting posts");
-		console.log(Posts.find({}, {sort: {postAt: 1}}).fetch());
-		return Posts.find({}, {sort: {postAt: 1}}).fetch();
+	getData: function() {
+		var url = "http://localhost:9000/post/getAllPosts";
+		fetchData(url);
 	},
 });
