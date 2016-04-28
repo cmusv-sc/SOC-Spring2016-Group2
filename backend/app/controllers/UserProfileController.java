fb3746bd8a08beaf042e7ecff3b3940bff2c725c
Thomas Hsu <thomas.adiuz@gmail.com>
2016-04-28 02:02:45 -0700

    Merged with origin. When I pulled this previous commit, I found that backend needs a very long time to start up. You can comment out line22, 23 and 24 in Global.java to prevent this. I hope the one who did this can fix it.

--- backend/app/controllers/UserProfileController.java
+++ backend/app/controllers/UserProfileController.java
@@ -205,7 +205,7 @@ public class UserProfileController extends Controller {
 
 		List<Author> authors = Author.findAll();
 
-
+		int i = 0;
 		for(Author author : authors){
 			String userName = author.getName();
 			Long authorId = Author.find_Author_Id(userName).get(0);
@@ -219,6 +219,8 @@ public class UserProfileController extends Controller {
 					subscribeNumbers,
 					TagsResult,
 					titleResult).save();
+			System.out.println("Generating User Profile " + i);
+			i ++;
 		}
 		return;
 	}
